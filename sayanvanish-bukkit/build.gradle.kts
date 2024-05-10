ddac8af76c360af755e0bce55ca6595132c080a4
Syrent <Syrent2356@Gmail.com>
2024-05-10 16:13:13 +0330

    ci: update stickynote

--- sayanvanish-bukkit/build.gradle.kts
+++ sayanvanish-bukkit/build.gradle.kts
@@ -27,8 +27,8 @@ dependencies {
 //    compileOnly("org.spigotmc:spigot-api:1.20.6-R0.1-SNAPSHOT")
     paperweight.paperDevBundle("1.20.4-R0.1-SNAPSHOT")
 
-    implementation("org.sayandev:stickynote-core:1.0.18")
-    implementation("org.sayandev:stickynote-bukkit:1.0.18")
+    implementation("org.sayandev:stickynote-core:1.0.19")
+    implementation("org.sayandev:stickynote-bukkit:1.0.19")
 
     implementation(project(":sayanvanish-api"))
     api(kotlin("reflect"))
