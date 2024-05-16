b86b0bc86a5310075b3f258c961eea50384cc971
Syrent <Syrent2356@Gmail.com>
2024-05-16 09:11:02 +0330

    ci: version increment

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,7 +13,7 @@ description = "A modular vanish system for Minecraft servers"
 
 allprojects {
     group = "org.sayandev"
-    version = "1.0.0-beta.3"
+    version = "1.0.0-beta.4"
 
     plugins.apply("java-library")
     plugins.apply("maven-publish")
